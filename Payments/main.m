59d40bd793f0692a34284ddd6b8e557260404f7a
Aaron Johnson <aaron9johnson@gmail.com>
2017-10-05 19:10:47 -0700

    user choice

--- Payments/main.m
+++ Payments/main.m
@@ -10,8 +10,14 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int cost = arc4random_uniform(900) + 100;
+        NSLog(@"Thank you for shopping at Acme.com\nYour total today is $%d\nPlease select your payment method:\n1: Paypal, 2: Stripe, 3: Amazon\n", cost);
+        char inputChars[255];
+        fgets(inputChars, 255, stdin);
+        NSString *inputString = [NSString stringWithCString:inputChars encoding:NSUTF8StringEncoding];
+        inputString = [inputString stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+        int choice = (int)[inputString integerValue];
+        NSLog(@"%d", choice);
     }
     return 0;
 }
